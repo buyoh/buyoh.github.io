cdee94a01910592ebe4ecf18102a32434b083d5d
buyoh <japanjetone@live.jp>
2016-08-04 17:54:57 +0900

    dic ver0.0.0

---
+++ webtool/dic/dic.js
@@ -0,0 +1,198 @@
+
+userconfig={
+	"date":(new Date()).getTime(),
+	"words":[
+		{
+			"t":"approximately",
+			"d":"近似"
+		},
+		{
+			"t":"truncate",
+			"d":"切り捨て"
+		},
+		{
+			"t":"independently",
+			"d":"独立して、無関係に"
+		},
+		{
+			"t":"criteria",
+			"d":"基準、尺度(単:criterion)"
+		},
+		{
+			"t":"variant",
+			"d":"確率"
+		}
+	]
+};
+userconfig.words.sort(function(l,r){return l.t>r.t;});
+
+wordlistindex=null;
+wordlistdom=null;
+
+domTemplate={
+	"word_dt":$("<dt></dt>"),
+	"word_dd":$("<dd></dd>")
+};
+
+// initialize
+$(document).ready(function(){
+	$("#input_keyword").on("keyup",function(){updateList();});
+
+	$("#button_appenditem").on("click",function(){appendNewWordToList();});
+	$("#button_removeitem").on("click",function(){removeSelectedWordFromList();});
+
+	loadConfigFromStorage();
+	userconfig.words.sort(function(l,r){return l.t>r.t;});
+	makeListFromConfig();
+
+	setInterval(function(){saveBackup();},1000*600);
+});
+
+$(window).on("unload",function(){
+	saveBackup();
+});
+
+function saveBackup(){
+	storeConfigToStorage();
+}
+
+function loadConfigFromStorage(){
+	try{
+		var r=JSON.parse(localStorage.myDictionary);
+		userconfig=r;
+		console.info("Success loading local settings");
+	}catch(e){
+		console.error("!!Failed loading local settings ->"+e);
+	}
+}
+
+function storeConfigToStorage(){
+	try{
+		userconfig.date=(new Date()).getTime();
+		localStorage.myDictionary=JSON.stringify(userconfig);
+		console.info("Success storing local settings");
+	}catch(e){
+		console.error("!!Failed storing local settings ->"+e);
+	}
+}
+
+function clearConfigAll(){
+	localStorage.clear();
+}
+
+function makeListFromConfig(){
+	var domlist=$("#div_wordlist");
+	domlist.empty();
+	for (var i=0;i<userconfig.words.length;i++){
+		var word=userconfig.words[i];
+		domlist.append(domTemplate.word_dt.clone().text(word.t).data("idx",i).on("click",call_selectWord));
+		domlist.append(domTemplate.word_dd.clone().text(word.d));
+	}
+}
+
+function updateList(){
+	var word=$("#input_keyword").val();
+	
+	var w=userconfig.words;
+	var l=0,h=w.length-1,m;
+	while (l<h){
+		m=Math.floor((l+h)/2);
+		if (w[m].t<word){
+			l=m+1;
+		}else if (word<w[m].t){
+			h=m-1;
+		}else{
+			l=m;h=m;
+		}
+	}
+	if (h<0)h=0;
+	if (w.length<=h) h=w.length-1;
+
+	if (word === w[h].t){
+		$("#button_removeitem").removeAttr("disabled");
+
+		if (wordlistdom!==null){
+			wordlistdom.removeClass("bg-success");
+			wordlistdom.next().removeClass("bg-success");
+		}
+
+		wordlistindex=h;
+		wordlistdom=$("#div_wordlist > dt").eq(wordlistindex);
+
+		wordlistdom.addClass("bg-success");
+		wordlistdom.next().addClass("bg-success");
+	}else{
+		$("#button_removeitem").attr("disabled","disabled");
+
+		if (wordlistdom!==null){
+			wordlistdom.removeClass("bg-success");
+			wordlistdom.next().removeClass("bg-success");
+		}
+		wordlistindex=null;
+		wordlistdom=null;
+	}
+
+	// console.log($("#div_wordlist > dt").eq(h).html());
+	scrollTo($("#div_wordlist > dt").eq(h));
+}
+
+function appendNewWordToList(){
+	var word=$("#input_keyword").val();
+	var description=$("#input_description").val();
+	if (word===null || word==="" || description===null || description==="")
+		return;
+
+	var json={"t":word,"d":description};
+
+	var w=userconfig.words;
+	var l=0,h=w.length-1,m;
+	while (l<h){
+		m=Math.floor((l+h)/2);
+		if (w[m].t<word){
+			l=m+1;
+		}else if (word<w[m].t){
+			h=m-1;
+		}else{
+			l=m;h=m;
+		}
+	}
+
+	if (word===w[l].t){
+		w[l].d=description;
+		$("#div_wordlist > dd").eq(wordlistindex).text(description);
+	}else{
+		w.splice(l+(word<w[l].t ? 0 : 1),0,json);
+		makeListFromConfig();
+	}
+	
+}
+
+function removeSelectedWordFromList(){
+	if (wordlistdom===null) return;
+	if (!confirm("<< alert (remove) >>")) return;
+	
+	userconfig.words.splice(wordlistindex,1);
+	wordlistdom.next().remove();
+	wordlistdom.remove();
+
+	$("#button_removeitem").attr("disabled","disabled");
+}
+
+
+function call_selectWord(){
+	var dom=$(this);
+	var idx=dom.data("idx");
+
+	$("#input_keyword").val(userconfig.words[idx].t);
+	$("#input_description").val(userconfig.words[idx].d);
+
+	updateList();
+
+	return ;
+}
+
+function scrollTo(dom){
+	var p=dom.offset().top-300;
+	$('html,body').animate({ scrollTop:p },'fast');
+}
+
