02344f1f5c124bb7414aee3ffa486f5308ba473b
buyoh <japanjetone@live.jp>
2017-04-10 23:58:09 +0900

    reversi まじめ

---
+++ webtool/reversi/ai/randomAI.js
@@ -0,0 +1,31 @@
+
+// ===============================
+// reversi RANDOM AI
+// ===============================
+appendAI(function(){
+
+    this.setup = function(gamerule){
+        // 開始時に実行するコードを書く．
+        // gamerule = {width : Number , height : Number}
+        // グローバル変数のgameruleと同じものが与えられる．
+
+    }
+
+    this.action = function(field, turn, hints){
+        // 番が回ってきたときに実行するコードを書く．
+        // field : 二次元配列(field[y][x] 1 -> 黒, 0 -> 空 , -1 -> 白)
+        // turn : 自分の石の色(1 or -1)
+        // hints : 二次元配列(hints[y][x] === 置くことができる?)
+
+        // x,yを石を置きたい座標としたハッシュ{'x':x, 'y':y}を出力する．
+
+        arr=[];
+        for (var y = 0; y < gamerule.height; ++y){
+            for (var x = 0;x < gamerule.width; ++x){
+                if (hints[y][x]) arr.push({'y':y,'x':x});
+            }
+        }
+        return arr[Math.floor(Math.random()*arr.length)];
+    }
+
+},'random');
---
+++ webtool/reversi/reversi.js
@@ -0,0 +1,484 @@
+
+/*
+TODO
+・
+
+既知バグ・不具合など
+・
+
+起こりそうなバグ・不具合など
+・瞬時に(2msぐらいで)複数のマスをクリックする
+
+*/
+
+var check_vertion = 'abc';
+
+var cvContext = null;
+var cvWidth, cvHeight;
+
+var buffContext = null;
+var buffCanvas = null;
+
+var display = {'board' : {
+                   'width' : 320,
+                   'height' : 320,
+                   'offsetX' : 20,
+                   'offsetY' : 20,
+                   'padding' : 0.25
+               },
+               'color' : {
+                   'background' : '#051',
+                   'border' : '#FFF',
+                   'stoneWhiteFill' : '#FFF',
+                   'stoneWhiteStroke' : '#888',
+                   'stoneBlackFill' : '#000',
+                   'stoneBlackStroke' : '#888',
+                   'cellHintsStroke' : '#8F3'
+               }};
+var gamerule = {'width' : 8,
+                'height' : 8,
+                'black' : 'u',
+                'white' : 'a0'};
+
+var game = {'field' : null,
+            'turn' : 1};
+
+var work = {'hints' : null,
+            'state' : 0 // for animation
+           };
+
+
+var effect = [];
+// effect.elem => {'type','timeLeft','onpaint':proc,'onfinish':proc}
+var effect_timerID = null;
+var effect_animInterval = 50;
+
+// work.state ... 0=>ready , 20=>thinking , 21=>userInput , 25=>animationTime , 50=>finish
+
+var ailist = [];
+
+// 初めに呼び出される
+$(document).ready(function(){
+	var canvasDom = $('#canvas_main')[0];
+	cvContext = canvasDom.getContext('2d');
+	cvWidth  = canvasDom.width;
+	cvHeight = canvasDom.height;
+
+    buffCanvas = $('<canvas></canvas>')[0];
+    buffCanvas.width = cvWidth; buffCanvas.height = cvHeight;
+    buffContext = buffCanvas.getContext('2d');
+
+    $('#canvas_main').on('contextmenu',function(e){clickready(e);return false;});
+    $('#canvas_main').on('click',clickready);
+    $('#canvas_main').on('mousemove',function(e){});
+
+    for (name in ailist){
+        $('#sel_type_black').append('<option value="'+name+'">AI ['+name+']</option>');
+        $('#sel_type_white').append('<option value="'+name+'">AI ['+name+']</option>');
+    }
+    if (1 <= ailist.length){
+        console.log($('#sel_type_black > option[value!=""]').eq(0));
+        $('#sel_type_black > option[value!=""]').eq(0).attr('selected','selected');
+    }
+
+    initialize_game();
+    updateInfomation();
+    paint();
+});
+
+
+function initialize_game(){
+    work.state = 0;
+
+    removeAllEffect();
+
+    // load gamerule
+    gamerule.black = $('#sel_type_black').val();
+    gamerule.black = gamerule.black == "" ? null : new ailist[gamerule.black];
+    gamerule.white = $('#sel_type_white').val();
+    gamerule.white = gamerule.white == "" ? null : new ailist[gamerule.white];
+
+    // initialize game
+    game.field = new Array(gamerule.height);
+    for (var y = 0; y < gamerule.height; ++y){
+        game.field[y] = new Array(gamerule.width);
+        for (var x = 0; x < gamerule.width; ++x){
+            if ((x==gamerule.width/2-1||x==gamerule.width/2)&&(y==gamerule.height/2-1||y==gamerule.height/2)){
+                game.field[y][x] = x == y ? -1 : 1;
+            }else{
+                game.field[y][x] = 0;
+            }
+        }
+    }
+    game.turn = 1;
+    work.hints = createHintsField(game.field, game.turn);
+
+    callNextPlayer();
+}
+
+
+// 石を置いた後の処理(ターン遷移，終了判定など)
+function continueGame(twice = false){
+    game.turn = -game.turn;
+    work.hints = createHintsField(game.field, game.turn);
+
+    var puttable = work.hints.reduce(function(s,e){return s+e.reduce(function(s,e){return s+e;});},0);
+
+    if (puttable == 0){
+        if (twice){
+            quitGame();
+            updateInfomation();
+            paint();
+        }else{
+            continueGame(true);
+        }
+        return;
+    }
+    updateInfomation();
+    paint();
+    setTimeout(function(){callNextPlayer();},0);
+}
+
+
+// 次のプレイヤーがすること
+// USERならばクリックされるまで待つ．
+// AIなら思考する．
+function callNextPlayer(){
+    work.state = 20; // thinking
+
+    var ctrl = 'u';
+    if (game.turn === 1){
+        ctrl = gamerule.black;
+    }else if (game.turn === -1){
+        ctrl = gamerule.white;
+    }
+    if (ctrl === null){
+        // user
+        work.state = 21; // userControl
+
+    }else{
+        // AI
+        setTimeout(function(){
+            var pos = ctrl.action(game.field, game.turn, work.hints);
+            // todo: check
+            setupEffect(20,{'oldField':copyMatrix(game.field)},function(){
+                continueGame();
+            });
+            putStone(game.field, game.turn, pos.x, pos.y);
+            updateInfomation();
+            paint();
+        },0);
+    }
+}
+
+
+function quitGame(){
+    removeAllEffect();
+    console.log('おしまい！');
+    alert('おしまい！');
+}
+
+
+function appendAI(ai, name){
+    ailist[name] = ai;
+}
+
+
+function button_restart(){
+    initialize_game();
+    updateInfomation();
+    paint();
+}
+
+
+function clickready(e){ // e.button : 0=left, 1=right
+    var x = e.offsetX;
+    var y = e.offsetY;
+    if (display.board.offsetX <= x && x < display.board.offsetX + display.board.width
+        && display.board.offsetY <= y && y < display.board.offsetY + display.board.height){
+        click_board((x-display.board.offsetX)*gamerule.width/display.board.width,
+                    (y-display.board.offsetY)*gamerule.height/display.board.height,e.button);
+    }
+}
+
+
+function click_board(x,y,button){
+    var lx = Math.floor(x);
+    var ly = Math.floor(y);
+    if (button === 2){
+        game.field[ly][lx] = (game.field[ly][lx] + 2) % 3 - 1;
+        work.hints = createHintsField(game.field, game.turn);
+    }else{
+        // is userControl state ?
+        if (work.state == 21){
+            if (work.hints[ly][lx]){
+                setupEffect(20,{'oldField':copyMatrix(game.field)},function(){continueGame();});
+                putStone(game.field,game.turn,lx,ly);
+                work.state = 25;
+            }
+        }
+    }
+    updateInfomation();
+    paint();
+}
+
+
+function updateInfomation(){
+    var str = "";
+    var score = 0;
+    var x,y;
+    for (y = 0; y < gamerule.height; ++y){
+        for (x = 0; x < gamerule.width; ++x){
+            score += game.field[y][x];
+        }
+    }
+    str+=game.turn == 1 ? "先手:黒\n" : "後手:白\n";
+    str+=`スコア差(黒-白) : ${score}\n`;
+    $('#infomation').val(str);
+}
+
+
+function removeAllEffect(){
+    if (effect_timerID !== null){
+        clearInterval(effect_timerID);
+        effect_timerID = null;
+    }
+    effect = [];
+}
+
+
+function setupEffect(type, data, proc){ // {'type','timeLeft','onpaint','onfinish':proc}
+    switch (type){
+        case 20: // flipping stone anim
+        effect.push({'type':type,'timeLeft':1000,'data':data,'onpaint':function(context,eff){
+            var x,y;
+            var nw,nh,w,h,ox,oy,srad,arc;
+            nw = gamerule.width;
+            nh = gamerule.height;
+            w = display.board.width;
+            h = display.board.height;
+            ox = display.board.offsetX;
+            oy = display.board.offsetY;
+            srad = Math.min(w/nw,h/nh) * (1 - display.board.padding) / 2;
+            arc = 2*Math.PI*(eff.timeLeft/1000);
+            for (y = 0; y < gamerule.height; ++y){
+                for (x = 0; x < gamerule.width; ++x){
+                    if (eff.data.oldField[y][x] !== 0 &&eff.data.oldField[y][x] != game.field[y][x] ){
+                        // todo:
+                        if (eff.data.oldField[y][x] == 1){
+                            buffContext.fillStyle = display.color.stoneBlackFill;
+                            buffContext.strokeStyle = display.color.stoneBlackStroke;
+                        }else if (eff.data.oldField[y][x] == -1){
+                            buffContext.fillStyle = display.color.stoneWhiteFill;
+                            buffContext.strokeStyle = display.color.stoneWhiteStroke;
+                        }
+                        buffContext.beginPath();
+                        buffContext.arc(ox+w*(2*x+1)/nw/2, oy+h*(2*y+1)/nh/2, srad,0,arc);
+                        buffContext.lineTo(ox+w*(2*x+1)/nw/2, oy+h*(2*y+1)/nh/2);
+                        buffContext.fill();
+
+                    }
+                }
+            }
+        },'onfinish':proc});
+        break;
+    }
+    if (0 < effect.length && effect_timerID === null){
+        effect_timerID = setInterval(effectInterval, effect_animInterval);
+    }
+}
+
+
+function effectInterval(){
+    // TODO: 時間計測? nowtime-begintime
+    paint();
+    for (var i = 0; i < effect.length; ++i){
+        var elem = effect[i];
+        elem.timeLeft -= effect_animInterval;
+        if (elem.timeLeft <= 0){
+            setTimeout(elem.onfinish(),0);
+            effect.splice(i,1);
+            --i;
+        }
+    }
+
+    if (effect.length === 0){
+        clearInterval(effect_timerID);
+        effect_timerID = null;
+    }
+}
+
+
+// 描画呼び出し
+function paint(){
+	buffContext.fillStyle = '#FFF';
+	buffContext.fillRect(0, 0, cvWidth, cvHeight);
+    
+    drawGame();
+    effect.forEach(function(elem,index,arr){
+        elem.onpaint(buffContext,elem);
+    });
+
+    cvContext.drawImage(buffCanvas, 0, 0);
+}
+
+
+function drawGame(){
+    var x,y;
+    var nw,nh,w,h,ox,oy,srad;
+    nw = gamerule.width;
+    nh = gamerule.height;
+    w = display.board.width;
+    h = display.board.height;
+    ox = display.board.offsetX;
+    oy = display.board.offsetY;
+    srad = Math.min(w/nw,h/nh) * (1 - display.board.padding) / 2;
+
+	buffContext.fillStyle = display.color.background;
+	buffContext.fillRect(0, 0, cvWidth, cvHeight);
+
+    buffContext.strokeStyle = display.color.border;
+    for (y = 0; y < nh; ++y){
+        for (x = 0; x < nw; ++x){
+            buffContext.strokeRect(ox+w*x/nw, oy+h*y/nh, (w)/nw, (h)/nh);
+        }
+    }
+
+    for (y = 0; y < nh; ++y){
+        for (x = 0; x < nw; ++x){
+            if (game.field[y][x] !== 0){
+                if (game.field[y][x] == 1){
+                    buffContext.fillStyle = display.color.stoneBlackFill;
+                    buffContext.strokeStyle = display.color.stoneBlackStroke;
+                }else if (game.field[y][x] == -1){
+                    buffContext.fillStyle = display.color.stoneWhiteFill;
+                    buffContext.strokeStyle = display.color.stoneWhiteStroke;
+                }
+                buffContext.beginPath();
+                buffContext.arc(ox+w*(2*x+1)/nw/2, oy+h*(2*y+1)/nh/2, srad,0,2*Math.PI);
+                buffContext.fill();
+                buffContext.beginPath();
+                buffContext.arc(ox+w*(2*x+1)/nw/2, oy+h*(2*y+1)/nh/2, srad,0,2*Math.PI);
+                buffContext.stroke();
+
+            }else{
+                if (work.hints[y][x]){
+                    buffContext.strokeStyle = display.color.cellHintsStroke;
+                    buffContext.strokeRect(ox+w*x/nw+((w)/nw/2-srad), oy+h*y/nh+((h)/nh/2-srad), srad*2, srad*2);
+                }
+            }
+        }
+    }
+}
+
+// 石を置く．
+// 置けない場所に置こうとした場合の挙動の例外処理は行わない．
+// @return 反した数
+function putStone(field,turn,x,y){
+    function dfs(x,y,turn,vx,vy){
+        if (x < 0 || y < 0 || gamerule.width <= x || gamerule.height <= y) return false;
+        if (field[y][x] === 0) return false;
+        if (field[y][x] === turn) return 0;
+        var r = dfs(x+vx,y+vy,turn,vx,vy);
+        if (r !== false){
+            field[y][x] = turn;
+            return r + 1;
+        }
+        return false;
+    }
+    field[y][x] = turn;
+    // memo: false+2+1 = 3
+    return  dfs(x-1,y-1,turn,-1,-1) + dfs(x  ,y-1,turn, 0,-1) + dfs(x+1,y-1,turn, 1,-1)
+          + dfs(x-1,y  ,turn,-1, 0) +                         + dfs(x+1,y  ,turn, 1, 0)
+          + dfs(x-1,y+1,turn,-1, 1) + dfs(x  ,y+1,turn, 0, 1) + dfs(x+1,y+1,turn, 1, 1);
+}
+
+
+function createHintsField(field,turn){
+    var result_hints = new Array(gamerule.height);
+    var i,x,y,l,r;
+    for (y = 0; y < gamerule.height; ++y){
+        result_hints[y] = new Array(gamerule.width);
+        for (x = 0; x < gamerule.width; ++x){
+            result_hints[y][x] = 0;
+        }
+    }
+    // yoko
+    for (y = 0; y < gamerule.height; ++y){
+        l = 0;
+        for (r = 0; r < gamerule.width; ++r){
+            if (field[y][r] === 0){ // empty
+                if (field[y][l] === turn){
+                    result_hints[y][r] += r-l-1;
+                }
+                l = r;
+            }else if (field[y][r] === turn){ // own
+                if (field[y][l] === 0){
+                    result_hints[y][l] += r-l-1;
+                }
+                l = r;
+            }
+        }
+    }
+    //tate
+    for (x = 0; x < gamerule.width; ++x){
+        l = 0;
+        for (r = 0; r < gamerule.height; ++r){
+            if (field[r][x] === 0){ // empty
+                if (field[l][x] === turn){
+                    result_hints[r][x] += r-l-1;
+                }
+                l = r;
+            }else if (field[r][x] === turn){ // own
+                if (field[l][x] === 0){
+                    result_hints[l][x] += r-l-1;
+                }
+                l = r;
+            }
+        }
+    }
+    // migishita
+    for (i = 3-gamerule.width; i < gamerule.height - 2; ++i){
+        l = Math.max(0,-i);
+        for (r = l; r < gamerule.width && r+i < gamerule.height; ++r){
+            if (field[i+r][r] === 0){ // empty
+                if (field[i+l][l] === turn){
+                    result_hints[i+r][r] += r-l-1;
+                }
+                l = r;
+            }else if (field[i+r][r] === turn){ // own
+                if (field[i+l][l] === 0){
+                    result_hints[i+l][l] += r-l-1;
+                }
+                l = r;
+            }
+        }
+    }
+    // migiue
+    for (i = 2; i < gamerule.height + gamerule.width - 3; ++i){
+        l = Math.max(0,i-gamerule.height+1);
+        for (r = l; r < gamerule.width && 0 <= i-r; ++r){
+            if (field[i-r][r] === 0){ // empty
+                if (field[i-l][l] === turn){
+                    result_hints[i-r][r] += r-l-1;
+                }
+                l = r;
+            }else if (field[i-r][r] === turn){ // own
+                if (field[i-l][l] === 0){
+                    result_hints[i-l][l] += r-l-1;
+                }
+                l = r;
+            }
+        }
+    }
+    return result_hints;
+}
+
+
+function copyMatrix(mat){
+    var result = new Array(mat.length);
+    for (var i = 0; i < mat.length; ++i){
+        result[i] = mat[i].slice();
+    }
+    return result;
+}
